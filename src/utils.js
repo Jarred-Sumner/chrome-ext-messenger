dbd6d4ec43a52fccf12dc60294ec263489f27ee7
Jarred Sumner <jarred@jarredsumner.com>
2018-08-02 13:49:34 -0700

    try switching to webextension polyfill

--- src/utils.js
+++ src/utils.js
@@ -1,96 +1,109 @@
-'use strict';
+"use strict";
 
-const Logdown = require('logdown');
-
-const Constants = require('./constants.js');
+const Logdown = require("logdown");
+require("webextension-polyfill");
+const Constants = require("./constants.js");
 
 // NOTE: Until https://github.com/caiogondim/logdown.js/issues/82 is implemented.
-const loggerLog   = new Logdown({ prefix: 'messenger-log', markdown: false });
-const loggerInfo  = new Logdown({ prefix: 'messenger-info', markdown: false });
-const loggerWarn  = new Logdown({ prefix: 'messenger-warn', markdown: false });
-const loggerError = new Logdown({ prefix: 'messenger-error', markdown: false });
+const loggerLog = new Logdown({ prefix: "messenger-log", markdown: false });
+const loggerInfo = new Logdown({ prefix: "messenger-info", markdown: false });
+const loggerWarn = new Logdown({ prefix: "messenger-warn", markdown: false });
+const loggerError = new Logdown({ prefix: "messenger-error", markdown: false });
 // Disables all instances with the 'messenger' prefix, but don't disable those in the negation.
-Logdown.disable('messenger*', '-messenger-error', '-messenger-warn'/*, '-messenger-info', '-messenger-log'*/);
+Logdown.disable(
+  "messenger*",
+  "-messenger-error",
+  "-messenger-warn" /*, '-messenger-info', '-messenger-log'*/
+);
 
 const Utils = {
-    log: function(level) {
-        // Remove the 'level' argument.
-        var loggerArgs = Array.prototype.slice.call(arguments, 1);
+  log: function(level) {
+    // Remove the 'level' argument.
+    var loggerArgs = Array.prototype.slice.call(arguments, 1);
 
-        switch (level) {
-            case 'log': {
-                loggerLog.log.apply(loggerLog, loggerArgs);
-                break;
-            }
-            case 'info': {
-                loggerInfo.info.apply(loggerInfo, loggerArgs);
-                break;
-            }
-            case 'warn': {
-                loggerWarn.warn.apply(loggerWarn, loggerArgs);
-                break;
-            }
-            case 'error': {
-                loggerError.error.apply(loggerError, loggerArgs);
+    switch (level) {
+      case "log": {
+        loggerLog.log.apply(loggerLog, loggerArgs);
+        break;
+      }
+      case "info": {
+        loggerInfo.info.apply(loggerInfo, loggerArgs);
+        break;
+      }
+      case "warn": {
+        loggerWarn.warn.apply(loggerWarn, loggerArgs);
+        break;
+      }
+      case "error": {
+        loggerError.error.apply(loggerError, loggerArgs);
 
-                // Abort execution on error.
-                throw 'Messenger error occurred, check more information above...';
-            }
+        // Abort execution on error.
+        throw "Messenger error occurred, check more information above...";
+      }
 
-            default: {
-                loggerError.error('Unknown log level: ' + level);
-                break;
-            }
-        }
-    },
-
-    // For each function:
-    // - Autobinding to ensure correct 'this' from all types of function invocations.
-    // - Add log level logging.
-    constructorTweakMethods: function(filename, thisObj) {
-        let wrapMethod = function(methodName) {
-            let origFunc = thisObj[methodName];
-
-            thisObj[methodName] = function() {
-                loggerLog.log('[' + filename + ':' + methodName + '()]', arguments);
-
-                return origFunc.apply(thisObj, arguments);
-            }.bind(thisObj);
-        };
-
-        for (let key in thisObj) {
-            if (typeof thisObj[key] === 'function') {
-                wrapMethod(key);
-            }
-        }
-    },
-
-    // TODO: export to npm package... chrome-ext-context/scope
-    getCurrentExtensionPart: function() {
-        let retVal;
-
-        if (typeof(chrome) !== 'undefined') {
-            // chrome.devtools is available in devtools panel.
-            // In latest Chrome, chrome.extension.getBackgroundPage() is available in background, popup & devtools.
-            if (chrome.devtools) {
-                retVal = Constants.DEVTOOL;
-            } else if (chrome.extension && typeof chrome.extension.getBackgroundPage === 'function') {
-                let backgroundPage = chrome.extension.getBackgroundPage();
-                retVal = backgroundPage === window ? Constants.BACKGROUND : Constants.POPUP;
-            } else {
-                retVal = Constants.CONTENT_SCRIPT;
-            }
-        } else {
-            loggerError.error('Could not identify extension part... are you running in a chrome extension context?');
-        }
-
-        loggerLog.log('detected current extension part: ' + retVal);
-        return retVal;
-    },
-
-    removeMessengerPortNamePrefix: function(portName) {
-        return portName.replace(new RegExp('^' + Constants.MESSENGER_PORT_NAME_PREFIX), '');
+      default: {
+        loggerError.error("Unknown log level: " + level);
+        break;
+      }
     }
+  },
+
+  // For each function:
+  // - Autobinding to ensure correct 'this' from all types of function invocations.
+  // - Add log level logging.
+  constructorTweakMethods: function(filename, thisObj) {
+    let wrapMethod = function(methodName) {
+      let origFunc = thisObj[methodName];
+
+      thisObj[methodName] = function() {
+        loggerLog.log("[" + filename + ":" + methodName + "()]", arguments);
+
+        return origFunc.apply(thisObj, arguments);
+      }.bind(thisObj);
+    };
+
+    for (let key in thisObj) {
+      if (typeof thisObj[key] === "function") {
+        wrapMethod(key);
+      }
+    }
+  },
+
+  // TODO: export to npm package... chrome-ext-context/scope
+  getCurrentExtensionPart: function() {
+    let retVal;
+
+    if (typeof chrome !== "undefined") {
+      // backgrounddevtools is available in devtools panel.
+      // In latest Chrome, backgroundextension.getBackgroundPage() is available in background, popup & devtools.
+      if (browser.devtools) {
+        retVal = Constants.DEVTOOL;
+      } else if (
+        browser.extension &&
+        typeof browser.extension.getBackgroundPage === "function"
+      ) {
+        let backgroundPage = browser.extension.getBackgroundPage();
+        retVal =
+          backgroundPage === window ? Constants.BACKGROUND : Constants.POPUP;
+      } else {
+        retVal = Constants.CONTENT_SCRIPT;
+      }
+    } else {
+      loggerError.error(
+        "Could not identify extension part... are you running in a chrome extension context?"
+      );
+    }
+
+    loggerLog.log("detected current extension part: " + retVal);
+    return retVal;
+  },
+
+  removeMessengerPortNamePrefix: function(portName) {
+    return portName.replace(
+      new RegExp("^" + Constants.MESSENGER_PORT_NAME_PREFIX),
+      ""
+    );
+  }
 };
 
 export default Utils;
--- test/main.js
+++ test/main.js
@@ -1,62 +1,66 @@
-let sinon = require('sinon');
-let chrome = require('sinon-chrome'); // jshint ignore:line
-let assert = require('assert');
-let rewire = require('rewire');
+let sinon = require("sinon");
+let chrome = require("sinon-chrome"); // jshint ignore:line
+let assert = require("assert");
+let rewire = require("rewire");
 
 // NOTE: rewire for accessing all private members and mocking them.
 // TODO: check issue https://github.com/jhnns/rewire/issues/62 + https://github.com/speedskater/babel-plugin-rewire
-let Messenger = rewire('../src/messenger.js');
-let Utils = rewire('../src/utils.js');
-let Constants = rewire('../src/constants.js');
+let Messenger = rewire("../src/messenger.js");
+let Utils = rewire("../src/utils.js");
+let Constants = rewire("../src/constants.js");
 
-describe('first suite', function () {
-    before(function() {
-        global.chrome = chrome;
+describe("first suite", function() {
+  before(function() {
+    global.chrome = chrome;
+  });
+
+  beforeEach(function() {
+    chrome.flush();
+  });
+
+  after(function() {
+    chrome.flush();
+    delete global.chrome;
+  });
+
+  it("first test", function() {
+    // simulate content script
+    // -------------------------
+    sinon.stub(Utils, "getCurrentExtensionPart", function() {
+      return Constants.CONTENT_SCRIPT;
     });
+    Messenger.__set__("Utils", Utils);
+    // -------------------------
 
-    beforeEach(function() {
-        chrome.flush();
-    });
+    // Init connection mock
+    // -------------------------
+    let mockPort = {
+      onMessage: {
+        addListener: sinon.stub()
+      },
 
-    after(function() {
-        chrome.flush();
-        delete global.chrome;
-    });
+      postMessage: sinon.stub()
+    };
 
-    it('first test', function() {
-        // simulate content script
-        // -------------------------
-        sinon.stub(Utils, 'getCurrentExtensionPart', function() {
-            return Constants.CONTENT_SCRIPT;
-        });
-        Messenger.__set__('Utils', Utils);
-        // -------------------------
+    chrome.runtime.connect.returns(mockPort);
+    // -------------------------
 
-        // Init connection mock
-        // -------------------------
-        let mockPort = {
-            onMessage: {
-                addListener: sinon.stub()
-            },
+    let messenger = new Messenger();
+    let connection = messenger.initConnection("some_name");
 
-            postMessage: sinon.stub()
-        };
+    assert.ok(
+      chrome.runtime.connect.withArgs({
+        name: Constants.MESSENGER_PORT_NAME_PREFIX + "some_name"
+      }).calledOnce
+    );
 
-        chrome.runtime.connect.returns(mockPort);
-        // -------------------------
+    assert.ok(mockPort.onMessage.addListener.calledOnce);
 
-        let messenger = new Messenger();
-        let connection = messenger.initConnection('some_name');
-
-        assert.ok(chrome.runtime.connect.withArgs({
-            name: Constants.MESSENGER_PORT_NAME_PREFIX + 'some_name'
-        }).calledOnce);
-
-        assert.ok(mockPort.onMessage.addListener.calledOnce);
-
-        assert.ok(mockPort.postMessage.withArgs({
-            from: Constants.CONTENT_SCRIPT,
-            type: Constants.INIT
-        }).calledOnce);
-    });
+    assert.ok(
+      mockPort.postMessage.withArgs({
+        from: Constants.CONTENT_SCRIPT,
+        type: Constants.INIT
+      }).calledOnce
+    );
+  });
 });
